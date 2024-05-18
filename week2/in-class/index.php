e93c9e3a50e56253b4df3c892060264f1f37dd64
pranjall <pranjalmlokhande@gmail.com>
2024-05-18 13:52:59 -0400

    added project files

---
+++ week2/in-class/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+
+    <?php
+
+
+
+    ?>
+    
+    <h1>
+        This is mh portfolio
+    </h1>
+    <p>
+    this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. this is paragraph 1. 
+    </p>
+    <ul>
+        <li>this is project 1</li>
+        <li>this is project 2</li>
+        <li>this is project 3</li>
+    </ul>
+    
+</body>
+</html>
